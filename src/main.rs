cfbcbe747a942908fdf0740ae7e0ff2a4f26c4a7
Miranquil <miranquil@gmail.com>
2023-06-13 19:46:11 +0800

    fulfill helps & status arg

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,31 @@
+use std::io::Write;
 use std::process::{Command, Stdio};
+use std::process;
 
 use clap::Parser;
 
+/// A filter for command line tools
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
+    /// Target binary name
     #[arg(long)]
     target: String,
+
+    /// Keyword of filter
     #[arg(long)]
     keyword: String,
+
+    /// If set, scgld will exit with target's status code
+    #[arg(long)]
+    status: bool,
+
+    /// Target arguments
     target_args: Vec<String>,
 }
 
 fn main() {
     let args = Args::parse();
-
 
     let target_args = args.target_args.join(" ");
     let target_child = Command::new(args.target)
@@ -22,14 +33,20 @@ fn main() {
         .stdout(Stdio::piped())
         .spawn()
         .expect("failed to execute ls");
+    let output = target_child.wait_with_output().expect("failed to get output");
+    let status_code = output.status.code().unwrap();
 
-    let grep_child = Command::new("grep")
+    let mut grep_child = Command::new("grep")
         .arg(args.keyword)
-        .stdin(Stdio::from(target_child.stdout.unwrap()))
+        .stdin(Stdio::piped())
         .spawn()
         .expect("failed to execute grep");
-
+    grep_child.stdin.as_mut().unwrap().write(&output.stdout).unwrap();
     let output = grep_child.wait_with_output().expect("failed to get output");
     let result = String::from_utf8(output.stdout).expect("failed to get utf8");
-    print!("{}", result)
+    print!("{}", result);
+
+    if args.status {
+        process::exit(status_code);
+    }
 }
